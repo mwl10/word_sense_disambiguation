ca486d232334cdc3b6b7081476beedd51fc0f209
mwl10 <matthewlowery@Matthews-MacBook-Pro.local>
2020-04-22 15:56:37 -0400

    add resnik_similarity.py

---
+++ resnik_similarity.py
@@ -0,0 +1,36 @@
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.corpus import wordnet_ic
+from nltk.corpus.reader.wordnet import information_content
+
+# procedure takes in two words as synsets
+
+def resnik_similarity(word1, word2):
+    brown_ic = wordnet_ic.ic('ic-brown-resnik-add1.dat')
+
+    subsumers = word1.common_hypernyms(word2)
+
+    if len(subsumers) == 0:
+        subsumer_ic = 0
+    else:
+        # get the information content value for the most specific/informative subsumer
+        subsumer_ic = max(wn.information_content(sub, brown_ic) for sub in subsumers)
+
+    return subsumer_ic
+
+dog = wn.synset('dog.n.01')
+cat = wn.synset('cat.n.01')
+hit = wn.synset('hit.v.01')
+slap = wn.synset('slap.v.01')
+
+resnik_similarity(dog, cat)
+resnik_similarity(hit, slap)
+
+
+
+
+
+
+
+
+
