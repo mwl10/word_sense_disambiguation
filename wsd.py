360d9c3159f74acd047e94ccb142d37f6a918a97
mwl10 <matthewlowery@Matthews-MacBook-Pro.local>
2020-04-24 13:51:03 -0400

    comment out wsd.py and finish its functionality

--- wsd.py
+++ wsd.py
@@ -2,28 +2,27 @@ import nltk
 from nltk.corpus import wordnet as wn
 from nltk.corpus import wordnet_ic
 from nltk.corpus.reader.wordnet import information_content
-import os
 import sys
 
-# procedure takes in two words as synset
-
+# grab command line arguments
 ic_file = sys.argv[1]
-wsd_test_file = open(sys.argv[2], "r+")
-results_out = open(sys.argv[3], "r+")
-
-## this needs to compute the most similar sense pair between the words and output
-## their similarity--> so we loop through each of them and grab the ones with the
+wsd_test_file = open(sys.argv[2], "r")
+results_out = open(sys.argv[3], "w")
 
 # here we add the ic parameter instead of directly injecting the
 # ic-brown-resnik-add1.dat file
 
-# NOT LIKING THE FIRST LINE
-
+# computes the resnik_similarity of the most similar sense pair of the two words given an input ic file
 def resnik_similarity(word1, word2, ic):
+    # initializing
     _ic = wordnet_ic.ic(ic)
     max_subsumer_ic = 0
     most_similar_syn_w1 = wn.synsets(word1)[0]
-    most_similar_syn_w2 = wn.synsets(word2)[0]
+
+    # if the word isn't in wordnet, ignore it
+    if len(wn.synsets(word2)) == 0:
+        most_similar_syn_w2 = 0
+    # double for loop to pick the most similar sense of each of the two words
     for w1_synset in wn.synsets(word1):
         for w2_sysnet in wn.synsets(word2):
             subsumers = w1_synset.common_hypernyms(w2_sysnet)
@@ -33,7 +32,7 @@ def resnik_similarity(word1, word2, ic):
             else:
                 # get the information content value for the most specific/informative subsumer
                 subsumer_ic = max(information_content(sub, _ic) for sub in subsumers)
-
+            # make sure we keep the most similar sense pair
             if max_subsumer_ic < subsumer_ic:
                 max_subsumer_ic = subsumer_ic
                 most_similar_syn_w1 = w1_synset
@@ -48,20 +47,17 @@ wtf_lines = wsd_test_file.readlines()
 # make sure results file is empty so we can write to it
 results_out.truncate(0)
 
-
-# want to grab the first word and compute the similarity of each of the ones
-# thereafter, printing as we go (on the same line) as
-
 # sift through the lines in the wsd test lines
 for line in wtf_lines:
     # get rid of the newline character
     line = line.split('\n')[0]
     # probe words and context words are separated by tab characters
-    list = line.split("\t")
+    list = line.split("  ")
     # grab the probe word
     probe_word = list[0]
     # grab the context words and split them according to the commas that separate them
-    context_list = line[1].split(",")
+    context_list = list[1].split(",")
+   # print(context_list)
 
     result_line = ""
     max_rs = 0
@@ -71,16 +67,21 @@ for line in wtf_lines:
         # synset relative to the probe word and the current context word
         rs, probe_syn, context_syn = resnik_similarity(probe_word, context_word, ic_file)
 
+        # write to the line in the form word1,word2,similarity
         result_line += '{},{},{}\t'.format(probe_word, context_word, rs)
 
-        # pick the sense based on which sense its chosen with the word
-        # that its most similar to, or the synset that is chosen the most
+        # pick the sense based on which sense is chosen with the word
+        # that its most similar to out of the context words
         if rs > max_rs:
             max_rs = rs
             preferred_sense = probe_syn
-    result_line += "\t{}".format(preferred_sense)
+    # write out the synset we picked
+    result_line += "{}\n".format(preferred_sense)
     results_out.write(result_line)
 
+
+wsd_test_file.close()
+results_out.close()
 
 # The intuition behind this algorithm is essentially the same intuition
 # exploited by Lesk (1986), Sussna (1993), and others: the most plausible
